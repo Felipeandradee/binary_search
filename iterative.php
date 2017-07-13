82a0069454e3aef7fde2044680f604d5192e8e62
Felipe Andrade <lipe.andradee1@gmail.com>
2017-07-13 08:39:18 -0300

    Adicionado para ler o array do arquivo

--- iterative.php
+++ iterative.php
@@ -1,52 +1,57 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: felipe
- * Date: 7/6/17
- * Time: 11:22 PM
- */
 
-$numbers = array(2, 3, 5, 4, 6, 13, 46);
 
-sort($numbers);
+$numbers = array();
+$f = fopen("f10000_3.txt", "r");
+
+// Lê cada uma das linhas do arquivo
+while(!feof($f)) {
+    $numbers[]= explode('; ', fgets($f))[1];
+}
+
+fclose($f);
+
+
+//$numbers = array(2, 3, 4, 5, 6, 13, 46,55);
+
+sort($numbers, SORT_NUMERIC);
+//print_r($numbers);
 
 function binary_search_iterative($elem, $array){
-    $top = sizeof($array) -1;
-    $bot = 0;
+    $top = sizeof($array) -1;   //g(2, 0, 0)
+    $bot = 0;                   //
 
-    while($top >= $bot)
+    while($bot <= $top) //gr(0, 0, 1)
     {
-        $p = floor(($top + $bot) / 2);
-        if ($array[$p] < $elem)
-            $bot = $p + 1;
-        elseif ($array[$p] > $elem)
-            $top = $p - 1;
+        $middle = floor(($top + $bot) / 2); //ex(1, 2, 0)
+        if ($elem == $array[$middle] ) { //ex(1, 0, 1)
+            return $middle;
+        }
+        if ($elem < $array[$middle]) //ex(1, 0, 1)
+            $top = $middle - 1; //ex(1, 1, 0)
         else
-            return $p;
+            $bot = $middle + 1; //ex(1, 1, 0)
     }
 
     return $elem.' not found.';
 }
 
-echo binary_search_iterative(3, $numbers);
-
-
 
 function binary_search_recursive($elem, $array, $bot, $top){
-    $p = floor(($top + $bot) / 2);
-    if($array[$p] == $elem)
-        return $p;
-    if($bot > $top)
+    $middle = floor(($top + $bot) / 2); // ex1(1, 2, 0)
+    if($array[$middle] == $elem) //g1(1, 0, 1)
+        return $middle;
+    if($bot > $top) //g2(0, 0, 1)
         return $elem.' not found.';
     else {
 
-        if ($array[$p] < $elem)
-            return binary_search_recursive($elem, $array, $p + 1, $top);
+        if ($array[$middle] < $elem) //g3(1, 0,1)
+            return binary_search_recursive($elem, $array, $middle + 1, $top); //ex2(0, 1, 0)
         else
-            return binary_search_recursive($elem, $array, $bot, $p - 1);
+            return binary_search_recursive($elem, $array, $bot, $middle - 1); //ex2(0, 1, 0)
     }
 }
 
-
+echo binary_search_iterative(562849, $numbers);
 echo "\n";
-echo binary_search_recursive(13, $numbers, 0, sizeof($numbers) -1);
+echo binary_search_recursive(562849, $numbers, 0, sizeof($numbers) -1);
